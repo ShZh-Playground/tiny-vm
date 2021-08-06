3f152d2eb7ba4fdb70b229d7821e2a08c99723a9
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-08-06 22:59:01 +0800

    test: test one calculation for our vm.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,15 @@ use vm::VM;
 fn main() {
     let mut chunks = chunk::Chunk::new();
     chunks.write_byte(123, OpCode::OpConstant);
-    chunks.write_constant(123, 64_f64);
+    chunks.write_constant(123, 2_f64);
+    chunks.write_byte(123, OpCode::OpConstant);
+    chunks.write_constant(123, 3_f64);
+    chunks.write_byte(123, OpCode::OpMultiplication);
+
+    chunks.write_byte(123, OpCode::OpConstant);
+    chunks.write_constant(123, 1_f64);
+    chunks.write_byte(123, OpCode::OpAddition);
+    chunks.write_byte(123, OpCode::OpNegative);
     chunks.write_byte(123, OpCode::OpReturn);
 
     let mut vm = VM::new(chunks);
