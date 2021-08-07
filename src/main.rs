0f5fe09cb3e974551320faf393abf9d83cd66b38
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-08-07 17:12:50 +0800

    feat: begin compiler's front end.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
 mod vm;
 mod error;
+mod scanner;
 
-use vm::{VM, chunk, opcode::OpCode};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, Read};
 
 fn main() {
-    let mut chunks = chunk::Chunk::new();
-    chunks.write_byte(123, OpCode::OpConstant);
-    chunks.write_constant(123, 2_f64);
-    chunks.write_byte(123, OpCode::OpConstant);
-    chunks.write_constant(123, 3_f64);
-    chunks.write_byte(123, OpCode::OpMultiplication);
-
-    chunks.write_byte(123, OpCode::OpConstant);
-    chunks.write_constant(123, 1_f64);
-    chunks.write_byte(123, OpCode::OpAddition);
-    chunks.write_byte(123, OpCode::OpNegative);
-    chunks.write_byte(123, OpCode::OpReturn);
-
-    let mut vm = VM::new(chunks);
-    vm.run();
+    // Pass cmd args to executable file by cargo: `cargo run -- [your flags]`
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        run_repl();
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        panic!("To many arguments!\n");
+    }
 }
+
+fn run_repl() {
+    loop {
+        let mut contents = String::new();
+        print!(">>> ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut contents).unwrap();
+        interprete(&contents);
+    }
+}
+
+fn run_file(path: &String) {
+    let mut file = File::open(path).expect("No such file!\n");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    interprete(&contents);
+}
+
+fn interprete(source: &String) {
+    println!("{}", source);
+}
+
